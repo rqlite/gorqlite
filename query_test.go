71722eb9d3221668b516748d51309bdb70605555
Reinaldy Rafli <aldy505@tutanota.com>
2022-07-09 22:32:13 +0700

    test: more query tests to ensure stability

--- query_test.go
+++ query_test.go
@@ -32,86 +32,144 @@ func TestQueryOne(t *testing.T) {
 		}
 	}
 
-	t.Logf("trying QueryOne")
-	qr, err := globalConnection.QueryOne("SELECT name, ts, wallet, bankrupt, payload FROM " + testTableName() + "_full WHERE id > 3")
-	if err != nil {
-		t.Errorf("failed during query: %v", err.Error())
-	}
+	t.Run("Normal case", func(t *testing.T) {
+		qr, err := globalConnection.QueryOne("SELECT name, ts, wallet, bankrupt, payload FROM " + testTableName() + "_full WHERE id > 3")
+		if err != nil {
+			t.Errorf("failed during query: %v", err.Error())
+		}
 
-	if qr.NumRows() != 2 {
-		t.Errorf("expected 2 row, got %v", qr.NumRows())
-	}
+		if qr.NumRows() != 2 {
+			t.Errorf("expected 2 row, got %v", qr.NumRows())
+		}
 
-	na := qr.Next()
-	if na != true {
-		t.Errorf("expected true, got %v", na)
-	}
+		if len(qr.Columns()) != 5 {
+			t.Errorf("expected 5 columns, got %v", len(qr.Columns()))
+		}
 
-	if qr.RowNumber() != 0 {
-		t.Errorf("expected row number to be 0, got %v", qr.RowNumber())
-	}
+		if len(qr.Columns()) == 5 {
+			expect := []string{"name", "ts", "wallet", "bankrupt", "payload"}
+			for i, c := range qr.Columns() {
+				if c != expect[i] {
+					t.Errorf("expected column %v to be %v, got %v", i, expect[i], c)
+				}
+			}
+		}
 
-	t.Logf("trying Map()")
-	r, err := qr.Map()
-	if err != nil {
-		t.Errorf("failed during map: %v", err.Error())
-	}
+		if len(qr.Types()) != 5 {
+			t.Errorf("expected 5 types, got %v", len(qr.Types()))
+		}
 
-	if r["name"].(string) != "Ferengi" {
-		t.Errorf("expected Ferengi, got %v", r["name"])
-	}
-	if ts, ok := r["ts"]; ok {
-		if ts, ok := ts.(time.Time); ok {
-			// time should not be zero because it defaults to current utc time
-			if ts.IsZero() {
-				t.Errorf("time should not be zero, got zero")
-			} else if ts.Before(started) {
-				t.Errorf("time %q is before start %q", ts, started)
+		na := qr.Next()
+		if na != true {
+			t.Errorf("expected true, got %v", na)
+		}
+
+		if qr.RowNumber() != 0 {
+			t.Errorf("expected row number to be 0, got %v", qr.RowNumber())
+		}
+
+		t.Logf("trying Map()")
+		r, err := qr.Map()
+		if err != nil {
+			t.Errorf("failed during map: %v", err.Error())
+		}
+
+		if r["name"].(string) != "Ferengi" {
+			t.Errorf("expected Ferengi, got %v", r["name"])
+		}
+		if ts, ok := r["ts"]; ok {
+			if ts, ok := ts.(time.Time); ok {
+				// time should not be zero because it defaults to current utc time
+				if ts.IsZero() {
+					t.Errorf("time should not be zero, got zero")
+				} else if ts.Before(started) {
+					t.Errorf("time %q is before start %q", ts, started)
+				}
+			} else {
+				t.Errorf("ts is a real %T", ts)
 			}
 		} else {
-			t.Errorf("ts is a real %T", ts)
+			t.Logf("--> FAILED: ts not found")
 		}
-	} else {
-		t.Logf("--> FAILED: ts not found")
-	}
 
-	t.Logf("trying Scan(), also float64->int64 in Scan()")
-	var id int64
-	var name string
-	var ts time.Time
-	var wallet float64
-	var bankrupt bool
-	var payload []byte
-	err = qr.Scan(&id, &name)
-	if err == nil {
-		t.Errorf("expected an error to be returned, got nil")
-	}
+		t.Logf("trying Scan(), also float64->int64 in Scan()")
+		var id int64
+		var name string
+		var ts time.Time
+		var wallet float64
+		var bankrupt bool
+		var payload []byte
+		err = qr.Scan(&id, &name)
+		if err == nil {
+			t.Errorf("expected an error to be returned, got nil")
+		}
 
-	err = qr.Scan(&name, &ts, &wallet, &bankrupt, &payload)
-	if err != nil {
-		t.Errorf("scanning: %v", err.Error())
-	}
-	if name != "Ferengi" {
-		t.Errorf("name should be 'Ferengi' but it's '%s'", name)
-	}
+		err = qr.Scan(&name, &ts, &wallet, &bankrupt, &payload)
+		if err != nil {
+			t.Errorf("scanning: %v", err.Error())
+		}
+		if name != "Ferengi" {
+			t.Errorf("name should be 'Ferengi' but it's '%s'", name)
+		}
 
-	qr.Next()
+		qr.Next()
 
-	if qr.RowNumber() != 1 {
-		t.Errorf("expected row number to be 1, got %v", qr.RowNumber())
-	}
+		if qr.RowNumber() != 1 {
+			t.Errorf("expected row number to be 1, got %v", qr.RowNumber())
+		}
 
-	err = qr.Scan(&name, &ts, &wallet, &bankrupt, &payload)
-	if err != nil {
-		t.Errorf("scanning: %s", err.Error())
-	}
-	if name != "Cardassian" {
-		t.Errorf("expected name to be 'Cardassian', got: %s", name)
-	}
+		err = qr.Scan(&name, &ts, &wallet, &bankrupt, &payload)
+		if err != nil {
+			t.Errorf("scanning: %s", err.Error())
+		}
+		if name != "Cardassian" {
+			t.Errorf("expected name to be 'Cardassian', got: %s", name)
+		}
 
-	if ts != meeting {
-		t.Errorf("expected ts to equal meeting, got ts: %v, meeting: %v", ts, meeting)
-	}
+		if ts != meeting {
+			t.Errorf("expected ts to equal meeting, got ts: %v, meeting: %v", ts, meeting)
+		}
+
+		if qr.Next() == true {
+			t.Errorf("expected no more rows, got one")
+		}
+	})
+
+	t.Run("Invalid Query", func(t *testing.T) {
+		qr, err := globalConnection.QueryOne("INVALID QUERY")
+		if err == nil {
+			t.Errorf("expected an error to be returned, got nil")
+		}
+
+		if qr.Err == nil {
+			t.Errorf("expected an error to be returned, got nil")
+		}
+	})
+
+	t.Run("Map before next", func(t *testing.T) {
+		qr, err := globalConnection.QueryOne("SELECT name  FROM " + testTableName() + "_full WHERE id = 3")
+		if err != nil {
+			t.Errorf("failed during query: %v - %v", err.Error(), qr.Err.Error())
+		}
+
+		_, err = qr.Map()
+		if err == nil {
+			t.Errorf("expected an error to be returned, got nil")
+		}
+	})
+
+	t.Run("Scan before next", func(t *testing.T) {
+		qr, err := globalConnection.QueryOne("SELECT name FROM " + testTableName() + "_full WHERE id = 3")
+		if err != nil {
+			t.Errorf("failed during query: %v - %v", err.Error(), qr.Err.Error())
+		}
+
+		var name string
+		err = qr.Scan(&name)
+		if err == nil {
+			t.Errorf("expected an error to be returned, got nil")
+		}
+	})
 }
 
 func TestQueryOneContext(t *testing.T) {
